e251b336b5b3b880c3205aad13c8a5b0917fee9b
kosCFS <kostiantyn@codefission.co.uk>
2025-11-07 17:10:58 +0200

    first commit

---
+++ src/App.jsx
@@ -0,0 +1,22 @@
+import { useEffect } from 'react';
+
+const APP_ID = import.meta.env.VITE_ONE_SIGNAL_APP_ID
+
+
+function App() {
+  useEffect(() => {
+    window.OneSignalDeferred = window.OneSignalDeferred || [];
+
+    window.OneSignalDeferred.push(async function (OneSignal) {
+      await OneSignal.init({
+        appId: APP_ID,
+        notifyButton: { enable: true },
+        allowLocalhostAsSecureOrigin: true, // важно для localhost
+      });
+    });
+  }, []);
+
+  return <div>React App</div>;
+}
+
+export default App;
